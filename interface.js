e07c545932296d277571e767ce98ef276c969097
孙达 <sunda@sundaMBA.local>
2019-03-19 18:40:26 +0800

    add api.ts

---
+++ interface.js
@@ -0,0 +1,12 @@
+function creatUser(num) {
+    if (num <= 0) {
+        return false;
+    }
+    var users = [];
+    for (var i = 0; i < num; i++) {
+        var users_1, i_1 = {}[0];
+        user[i_1][name] = 'user000' + i_1;
+    }
+    return users;
+}
+console.log(creatUser(2));
--- interface.ts
+++ interface.ts
@@ -18,14 +18,14 @@ interface Common{
     readonly delTime:Date,
     status:Array<number>,  
 }
-interface Toic extends Common{
+interface Topic extends Common{
     title:string,
     viewNum:number,
     joinNum:number,
     sumarry:string,
     hotFlag:boolean,
 }
-interface Contribute  extends Common{
+interface Post  extends Common{
     pic?:string,
     discussNum:number,
     likedNum:number,
@@ -48,16 +48,16 @@ interface Reply  extends Common{
     readonly toUser:string,      //关联被回复方userID，
 }
 
-function creatUser(){
-    return {
-        name:'Test',
-        pic:null,
-        sign:null,
-        followStatus:0, 
-        fansNum:0,
-        actiivtyNum:0,
-        d:'user01',
-        creatTime:function(){return Date();},
-    }
+interface HomePageResult {
+    topics: Topic[],
 }
-creatUser();
+
+interface DiscussResult {
+    discuss: Discuss,
+    reply: Reply[],
+}
+
+interface TopicDetailResult {
+    topic: Topic,
+    discusses: DiscussResult[],
+}
