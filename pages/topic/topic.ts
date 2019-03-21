289458f83b0ff54489ee83d0d86f7c5f89b095ca
GeGanYmx <2319649532@qq.com>
2019-03-21 18:50:23 +0800

    test ts

---
+++ pages/topic/topic.ts
@@ -0,0 +1,69 @@
+//index.js
+//获取应用实例
+import { IMyApp } from '../../app'
+
+//调用后台api
+/*import api from '../../api'
+import { ITopicDetailParams } from '../../api/types/http_msg';
+
+async function getTopic(obj:ITopicDetailParams):Promise<any>{
+  return await api.getTopic(obj);
+}
+
+let topic=getTopic({id:1}).then(top=>top).catch(()=>{console.log('后台正忙')})
+
+console.log(`测试：${topic}`);*/
+
+const app = getApp<IMyApp>()
+
+Page({
+  data: {
+    motto: '点击 “编译” 以构建',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad() {
+    if (app.globalData.userInfo) {
+      this.setData!({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true,
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = (res) => {
+        this.setData!({
+          userInfo: res,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData!({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+
+  getUserInfo(e: any) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData!({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
