20953cb1454caa11be9062da30c0b902a6354846
Gui Lin <guileen@gmail.com>
2019-03-21 16:36:40 +0800

    add api docs

--- api/index.ts
+++ api/index.ts
@@ -13,27 +13,42 @@ import {
 } from './types/http_msg'
 
 export default {
+  /**
+   * 获取首页话题列表
+   */
   getHomeTopicList: async (params: IHomeTopicListParams): Promise<IHomeTopicListResponse> => {
     return {
-      hasMore: Math.random() > 0.3,
+      cursor: Math.random() > 0.3 ? (params.cursor || 0) + 10 : 0,
       posts: mock.repeat(10, mock.post),
     }
   },
+  /**
+   * 获取投稿详情
+   */
   getPost: async (params: IPostDetailParams): Promise<IPostDetailResponse> => {
-    return mock.post()
+    return mock.post(params.id)
   },
+  /**
+   * 获取话题详情
+   */
   getTopic: async (params: ITopicDetailParams): Promise<ITopicDetailResponse> => {
     return {
       hasMore: Math.random() > 0.5,
       posts: mock.repeat(10, mock.post),
-      topic: mock.post(),
+      topic: mock.post(params.id),
     }
   },
+  /**
+   * 获取用户信息
+   */
   getUser: async (params: IUserPageParams): Promise<IUserPageResponse> => {
     return {
-      user: mock.user(),
+      user: mock.user(params.id),
     }
   },
+  /**
+   * 发布话题/投稿
+   */
   publishPost: async (params: IPostPublishParams): Promise<IPostPublishResponse> => {
     return mock.post()
   },
--- api/types/http_msg.ts
+++ api/types/http_msg.ts
@@ -1,29 +1,48 @@
 import { IPost, IThumbnail } from './posts'
 import { IUser } from './user'
 
+/**
+ * 错误返回
+ */
 export interface IHTTPErrorResponse {
-  code?: string,
-  message?: string,
+  code?: string                 // 错误码
+  message?: string              // 错误信息
 }
 
+/**
+ * 用户页面参数
+ */
 export interface IUserPageParams {
-  id: number
+  id: number                    // 用户ID
 }
 
+/**
+ * 用户页面结果
+ */
 export interface IUserPageResponse {
-  user: IUser,
+  user: IUser                   // 用户信息
 }
 
+/**
+ * 首页话题列表参数
+ */
 export interface IHomeTopicListParams {
-  sinceId?: number, // 从某个位置开始，默认0，从最上方开始
-  limit?: number, // 最多几条，默认20个
+  cursor?: number              // 从某个位置开始，默认0，从最上方开始
+  limit?: number                // 最多几条，默认20个
 }
 
+/**
+ * 首页话题列表结果
+ */
 export interface IHomeTopicListResponse {
-  posts: IPost[]
-  hasMore: boolean
+  posts: IPost[]                // 话题/投稿列表
+  // cursor 为0，则到达了尾页，没有更多数据。获取下一页的时候，将
+  cursor: number                // 下一页标志，传入cursor。0表示无下一页。
 }
 
+/**
+ * 发布话题/投稿参数
+ */
 export interface IPostPublishParams {
   title: string,            // 标题
   text: string,             // 内容
@@ -32,30 +51,51 @@ export interface IPostPublishParams {
   refPostId?: number,       // 参与的话题（可以扩展为引用他人的文章
 }
 
+/**
+ * 发布话题/投稿结果
+ */
 export type IPostPublishResponse = IPost
 
+/**
+ * 话题详情页参数
+ */
 export interface ITopicDetailParams {
   id: number
 }
 
+/**
+ * 话题详情页结果
+ */
 export interface ITopicDetailResponse {
   topic: IPost,
   posts: IPost[],
   hasMore: boolean,
 }
 
+/**
+ * 投稿详情页参数
+ */
 export interface IPostDetailParams {
   id: number
 }
 
+/**
+ * 投稿详情页结果
+ */
 export type IPostDetailResponse = IPost
 
+/**
+ * 回复列表参数
+ */
 export interface IReplyListParams {
   postId: number
   sinceId?: number,
   limit?: number
 }
 
+/**
+ * 回复列表结果
+ */
 export interface IReplyListResponse {
   replies: Reply[]
   hasMore: boolean
