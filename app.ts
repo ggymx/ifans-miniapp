40096d0d70a228bea968869659c4e51e4b8a7056
GeGanYmx <2319649532@qq.com>
2019-03-21 12:15:46 +0800

    init wexin project

--- app.ts
+++ app.ts
@@ -8,6 +8,7 @@ export interface IMyApp {
 
 App<IMyApp>({
   onLaunch() {
+    console.log('hello world')
     // 展示本地存储能力
     var logs: number[] = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
@@ -16,7 +17,7 @@ App<IMyApp>({
     // 登录
     wx.login({
       success(_res) {
-        // console.log(_res.code)
+        console.log(_res.code)
         // 发送 _res.code 到后台换取 openId, sessionKey, unionId
       }
     })
