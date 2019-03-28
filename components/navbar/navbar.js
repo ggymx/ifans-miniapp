03b847bb520eeaae2a99c0176d505eb07889e7b3
GeGanYmx <2319649532@qq.com>
2019-03-29 00:16:31 +0800

    add linkList

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -15,7 +15,7 @@ Component({
       type: Boolean,
       value: false
     },
-    gear:{
+    gear: {
       type: Boolean,
       value: false
     }
@@ -38,8 +38,60 @@ Component({
         delta: 1
       })
     },
-    backGear:function(){
-      console.log("测试List");
+    backGear: function () {
+      // console.log("测试List");
+      wx.showActionSheet({
+        itemList: ['我的首页', '消息通知', '我的足迹', '我的关注', '联系客服'],
+        success(res) {
+          switch (res.tapIndex) {
+            case 0:
+              wx.navigateTo({
+                url: '../my/my',
+                success: function () {
+                  wx.showToast({
+                    title: '跳转空间页！'
+                  });
+                }
+              });
+              break;
+            case 1:
+              wx.navigateTo({
+                url: '../news/news',
+                success: function () {
+                  wx.showToast({
+                    title: '跳转消息页！'
+                  });
+                }
+              });
+              break;
+
+            case 2:
+              wx.navigateTo({
+                url: '../track/track',
+                success: function () {
+                  wx.showToast({
+                    title: '跳转足迹页！'
+                  });
+                }
+              });
+              break;
+
+            case 3:
+              wx.navigateTo({
+                url: '../news-concern/news-concern',
+                success: function () {
+                  wx.showToast({
+                    title: '跳转关注页！'
+                  });
+                }
+              });
+              break;
+          }
+        },
+        fail(res) {
+          console.log(res.errMsg)
+        }
+      })
     }
   }
 })
