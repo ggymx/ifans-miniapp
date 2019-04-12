e8aa43cb10e977b1884946ebc69dcd9aa2f1b9bf
Gui Lin <guileen@gmail.com>
2019-04-12 20:45:19 +0800

    update navback

--- common/helper.ts
+++ common/helper.ts
@@ -4,12 +4,7 @@ export function isPostPage(page: string) {
 
 export function smartGotoPage(option: wx.NavigateToOption) {
   const pages = getCurrentPages()
-  // 页面栈已满，或，上三页、目标页 都是话题或投稿类型
-  if (pages.length === 10 || (pages.length >= 3 &&
-    isPostPage(pages[pages.length - 1].route) &&
-    isPostPage(pages[pages.length - 2].route) &&
-    isPostPage(pages[pages.length - 3].route) &&
-    isPostPage(option.url))) {
+  if (pages.length === 10) {
     wx.redirectTo(option)
   } else {
     wx.navigateTo(option)
--- components/navbar/navbar.ts
+++ components/navbar/navbar.ts
@@ -1,4 +1,5 @@
 import { IMyApp } from './../../app';
+import { isPostPage } from '../../common/helper';
 const app: IMyApp = getApp();
 /*待初始化的缓存和草稿 */
 let topicCache:any
@@ -45,27 +46,23 @@ Component({
            //回退时，如果缓存有话题和草稿，则清除
            if(topicCache) wx.removeStorageSync('topic')
            if(draft) wx.removeStorageSync('draft')
-          /*
           const pages = getCurrentPages().map(p => p.route)
           const currentPage = pages[pages.length - 1]
           let delta = 1
-          if (currentPage === 'pages/publisher/publisher' || currentPage === 'pages/topic-detail/topic-detail') {
+          if (isPostPage(currentPage)) {
             for (delta = 1; delta < pages.length; delta++) {
               let targetPage = pages[pages.length - delta - 1]
-              console.log('delta', delta, targetPage)
-              if (targetPage !== 'pages/publisher/publisher' && targetPage !== 'pages/topic-detail/topic-detail') {
+              if (!isPostPage(targetPage)) {
                 // 后退停在最初进入的那个 话题或投稿 页面。至少后退一页
-                if(delta > 1) {
+                if (delta > 1) {
                   delta--
                 }
                 break
               }
             }
           }
-          console.log('backDelta', delta)
-          */
             wx.navigateBack({
-                delta: 1
+                delta
             });
         },
         backGear() {
