6a585e69300c41a3289805459441ca94e49e6914
Li xuming <xuming.li@ifans.pub>
2019-05-13 20:51:26 +0800

    create and preview

--- pages/post/create.ts
+++ pages/post/create.ts
@@ -97,72 +97,30 @@ Page({
 
   /*发布话题 */
   titleParti(event: any) {
-    const that = this;
-    //获取缓存中的token，同步方式
-    const token = wx.getStorageSync('token');
-    if (!token) {
-      //用户未登录或者token过期
-      wx.showToast({ title: '请先登录！' });
-      setTimeout(() => {
-        smartGotoPage({
-          url: '../login'
-        });
-      }, 300);
-
-    } else {
-      if (!this.data.titleValue) {
-        wx.showToast({
-          icon: 'none',
-          title: '标题不能为空！'
-        });
-      } else if (this.data.titleValue.length < 5) {
-        wx.showToast({
-          icon: 'none',
-          title: '抱歉，标题不得低于五个字呦'
-        });
-      } else {
-        wx.showLoading({
-          title: '发布中...'
-        });
-        const userId = wx.getStorageSync('userId');
-        var gallery = that.data.image2Commit.map(item => item.imageURL).join(',');       //获取图片数据
-        api.request({
-          url: '/v1/post/create',
-          data: {
-            text: that.data.pushText,
-            title: that.data.titleValue,
-            type: 1,
-            gallery,    //
-          },
-          method: 'POST',
-          success(res) {
-            const data = res.data as any
-            const id = data.id;
-            setTimeout(() => {
-              wx.hideLoading({
-                success() {
-                  wx.showToast({
-                    title: '发布成功！'
-                  });
-                  //投稿成功则清除缓存中的话题和草稿
-                  wx.removeStorageSync('topic');
-                  wx.removeStorageSync('draft');
-                  console.log('===tId=====', id)
-                  setTimeout(() => {
-                    wx.redirectTo({
-                      url: `./topic-detail?id=${id}`
-                    });
-                  }, 200);
-                }
-              });
-
-            }, 200);
-
-          }
-        });
-
-      }
+    if (!this.data.titleValue) {
+      wx.showToast({
+        icon: 'none',
+        title: '标题不能为空！'
+      });
+      return
+    } else if (this.data.titleValue.length < 5) {
+      wx.showToast({
+        icon: 'none',
+        title: '抱歉，标题不得低于五个字呦'
+      });
+      return
     }
+
+    var gallery = this.data.image2Commit.map(item => item.imageURL).join(','); 
+    const postData = {
+      text: this.data.pushText,
+      title: this.data.titleValue,
+      type: 1,
+      gallery,    //
+    }
+    wx.navigateTo({
+      url: '../preview/preview?post='+encodeURIComponent(JSON.stringify(postData))
+    })
   },
   // titleFocus(event: any) {   //title 的view 也要一起改变大小
   //   this.setData!({
