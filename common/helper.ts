804047bbe43aca6661c7e523bcdc5b2e0a53b1a8
GuiLin <guileen@gmail.com>
2019-05-25 17:54:06 +0800

    add post-share-canvas

--- common/helper.ts
+++ common/helper.ts
@@ -10,6 +10,92 @@ export function smartGotoPage(option: wx.NavigateToOption) {
     wx.navigateTo(option)
   }
 }
+
+export function getImageInfo(src: string): Promise < wx.GetImageInfoSuccessCallbackResult > {
+  return new Promise<any>((success, fail) =>
+    wx.getImageInfo({ src, success, fail }));
+}
+
+/**
+ *
+ * @param ctx canvas 的ctx
+ * @param imgInfo 图像信息，通过 getImageInfo 获取
+ * @param dx
+ * @param dy
+ * @param dw
+ * @param dh
+ */
+export function drawImageCenterCrop(ctx: wx.CanvasContext, imgInfo: wx.GetImageInfoSuccessCallbackResult,
+  dx: number, dy: number, dw: number, dh: number) {
+    // biger is wider
+    const srcRatio = imgInfo.width/imgInfo.height
+    const dstRatio = dw/dh
+    let sx=0
+    let sy=0
+    let sw=imgInfo.width
+    let sh=imgInfo.height
+    if(srcRatio > dstRatio) {
+      // source is wider
+      sw = sh * dstRatio
+      sx = (imgInfo.width - sw)/2
+    } else {
+      sh = sw / dstRatio
+      sy = (imgInfo.height - sh)/2
+    }
+    (ctx as any).drawImage(imgInfo.path, sx, sy, sw, sh, dx, dy, dw, dh)
+}
+
+export function fillTextVerticalCenter(context, text, x, y, maxWidth, lineHeight) {
+  // text to words
+  const words = []
+  let lastWord = ''
+  for(let c of text){
+    let code = c.charCodeAt(0)
+    if(code >127) {
+      // 大于 127 的，算中文
+      if(lastWord) {
+        words.push(lastWord)
+      }
+      words.push(c)
+      lastWord = ''
+    } else if(c=== '\t' || c===' ' || c==='\r' || c=== '\n') {
+      // 空格/换行，词结束
+      if(lastWord) {
+        words.push(lastWord)
+      }
+      lastWord = c
+    } else {
+      // ascii码，算英文，整个单词一个词
+      lastWord += c
+    }
+  }
+  if (lastWord) {
+    words.push(lastWord)
+  }
+
+  // words to lines
+  const lines = []
+  let line = ''
+  for (let n = 0; n < words.length; n++) {
+    const testLine = line + words[n];
+    const metrics = context.measureText(testLine);
+    const testWidth = metrics.width;
+    if (testWidth > maxWidth && n > 0) {
+      lines.push(line)
+      line = words[n];
+    } else {
+      line = testLine;
+    }
+  }
+  lines.push(line)
+  // draw lines
+  let lineY = y - (lines.length-1)*lineHeight/2
+  for (const line of lines) {
+    context.fillText(line, x, lineY, maxWidth);
+    lineY+= lineHeight
+  }
+}
+
  export function smartDate(option: any){
   return option
  }
---
+++ components/post-share-canvas/index.ts
@@ -0,0 +1,90 @@
+import { IPost } from '../../common/types/posts'
+import { getImageInfo, drawImageCenterCrop } from '../../common/helper';
+import { fillTextVerticalCenter } from './../../common/helper';
+
+Component({
+  properties: {
+    post: {
+      type: Object,
+      value: {},
+      observer() {
+        (this as any).createShareImage();
+      },
+    },
+  },
+  onCanvasError(e) {
+    console.log('in base canvas error', e)
+  },
+  data: {
+    shareMessageInfo: {},
+    picPath: ''
+  },
+  methods: {
+    onCanvasError(e) {
+      console.log('in methods canvas error', e)
+    },
+    // 生成分享图
+    async createShareImage() {
+      if(!this.data.post || !this.data.post.id) { return }
+      console.log('createShareImage', this.data.post)
+      // @ts-ignored
+      const ctx: any = wx.createCanvasContext('myCanvas', this)
+      //防止加载中canvas上下文没有生成
+      if (!ctx) {
+        return;
+      }
+      // 绘制主体
+      await this.drawImageAndText(ctx)
+      const that = this
+      //canvas转图片
+      ctx.draw(true, () => {
+        console.log('draw output')
+        setTimeout(() => {
+          (wx as any).canvasToTempFilePath({
+            quality: 90,
+            canvasId: 'myCanvas',
+            success: (res) => {
+              console.log('picready', res.tempFilePath)
+              that.triggerEvent('picready', { picPath: res.tempFilePath });
+            }
+          }, that);
+        }, 300);
+      });
+    },
+    //合并文字和图案
+    async drawImageAndText(ctx: any) {
+      const post: IPost = this.data.post
+      const w = this.autoSize(420)
+      const h = this.autoSize(336)
+      if(post.thumbnails && post.thumbnails.length > 0) {
+        let imgPath = post.thumbnails[0].image
+        let imgInfo = await getImageInfo(imgPath)
+        drawImageCenterCrop(ctx, imgInfo, 0, 0, w, h)
+      }
+      ctx.fillStyle = 'rgba(0,0,0,0.2)'
+      ctx.fillRect(0, 0, w, h)
+
+      ctx.setFillStyle('#fff');
+      ctx.setFontSize(32);
+
+      ctx.textAlign = 'center'
+      ctx.textBaseline = 'middle'
+      fillTextVerticalCenter(ctx, post.title || post.text, w/2, h/2, this.autoSize(300), 32)
+      let imgInfo = await getImageInfo(post.user.avatar)
+      let aw = this.autoSize(51)
+      drawImageCenterCrop(ctx, imgInfo, w-aw,h-aw, aw, aw)
+      ctx.textAlign = 'right'
+      ctx.setFontSize(16)
+      ctx.fillText(post.user.nickname, w-aw, h-aw/2)
+      console.log('draw end')
+      // draw center red point
+      // ctx.fillStyle = '#f00'
+      // ctx.fillRect(w/2-1, h/2-1, 2,2)
+    },
+    //图片尺寸兼容
+    autoSize(num) {
+      const scale = wx.getSystemInfoSync().windowWidth / 375;
+      return num * scale
+    }
+  }
+});
