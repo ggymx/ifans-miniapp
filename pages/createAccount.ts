7a1c391a2883791ac62ab39b117cf6f5b8763ccd
Vito <vito.ge@ifans.pub>
2019-05-29 19:13:23 +0800

    添加账号列表和创建小号功能

--- pages/createAccount.ts
+++ pages/createAccount.ts
@@ -3,87 +3,54 @@
 import { IMyApp } from '../app'
 import api from '../common/api';
 import { smartGotoPage } from '../common/helper';
+import { chooseImage } from '../common/upload';
 
 const app = getApp<IMyApp>()
 let loginCode: string
 Page({
   data: {
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    returnInfo: null,
-    statusText: '授权登录'
+    ID: '',
+    upLoadImage: null
   },
-  /*授权登录 */
-  onGotUserInfo(e: any) {
-    /*点击确认按钮 */
-    /*当拿到用户信息时*/
-    if (e.detail.userInfo) {
-      const that = this;
-      /*这里不需要用token判断 */
+  //上传头像
+  async uploadAvatar() {
+    const upLoadImage = await chooseImage(1);
+    this.setData({
+      upLoadImage
+    })
+    console.log('上传图片的路径--------------', upLoadImage);
+  },
+  inputID(e: any) {
+    console.log('用户的昵称------------------', e.detail.value)
+    this.setData({
+      ID: e.detail.value
+    })
+  },
+  //创建用户
+  confirmCreate() {
+    if (this.data.ID && this.data.upLoadImage) {
+      const that = this as any;
+      const userId = wx.getStorageSync('userId');
       api.request({
-
-        url: '/v1/auth/login',
-
+        url: '/v1/user/create-mini',
         data: {
-          code: loginCode,
-          encryptedData: e.detail.encryptedData,
-          iv: e.detail.iv,
-          rawData: e.detail.rawData,
-          signature: e.detail.signature,
+          nickname: this.data.ID,
+          avatar: this.data.upLoadImage,
+          regInfo: 'wxapp',
+          rootuid: userId
         },
-
-        header: {
-          'content-type': 'application/json'
-        },
-
         method: 'POST',
-
-        success(res) {
-          const data = res.data as any
-          that.setData!({
-            returnInfo: res.data,
-            statusText: '已登录'
-          });
-          //往缓存中添加token（异步）
-          api.setToken(data.token);
-          //往缓存中添加当前用户的id
-          wx.setStorage({
-            key: 'userId',
-            data: data.user.id
-          });
+        success(res:any) {
+          console.log('返回的数据', res);
+          if(res&&res.data.id){
+            wx.showToast({ title: '创建成功！' });
+            setTimeout(() => {
+              wx.navigateBack({ delta: 2 });
+            }, 200);
+          }
         }
       });
-      wx.showLoading({
-        title: '加载中'
-      });
-      setTimeout(() => {
-        wx.hideLoading({
-          success() { wx.navigateBack({ delta: 1 }) }
-        });
-      }, 500);
-      /*发起http请求-插入数据 */
 
-    } else {
-      wx.showModal({
-        title: '警告',
-        content: '您点击了拒绝授权，将无法进入小程序，请授权之后再进入!!!',
-        showCancel: false,
-        confirmText: '返回授权'
-      })
     }
-  },
-
-  onLoad() {
-    /*获取登录凭证*/
-    wx.login({
-      success(res) {
-        /*wx.login获取code-微信登录的标识 */
-        loginCode = res.code
-      }
-    })
-
-  },
-   //图片预览
-   imgPre(event: any){
-      console.log('测试------');
-},
+  }
 })
